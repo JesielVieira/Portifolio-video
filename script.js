cd0fc0a962dd252ad17ec83e44bc3a4b8d554eb6
Jesiel Vieira <jesielvieira07@gmail.com>
2025-04-12 01:18:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+function selecao() {
+   var audio = document.getElementById("selecao-song");
+    audio.play()
+    audio.currentTime = 0;
+    audio.volume = 0.01;
+}
+
+function selecaocard() {
+    var audio = document.getElementById("clique-selecao");
+     audio.play()
+     audio.currentTime = 0;
+        audio.volume = 0.1;
+    
+ }
+
+ function selecaotipo() {
+    var audio = document.getElementById("selecaotipo");
+     audio.play()
+     audio.currentTime = 0;
+        audio.volume = 0.01;
+    
+ }
+
+ 
+ function click() {
+    var audio = document.getElementById("click");
+     audio.play()
+     audio.currentTime = 0;
+        audio.volume = 0.05;
+
+ }
+
+
+ function videocardover() {
+   var audio = document.getElementById("videocardover");
+    audio.play()
+    audio.currentTime = 0;
+       audio.volume = 0.2;
+   
+}
+
+
